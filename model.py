d115575f200356dc574369602eda56e612e44678
lcukyfuture <48312954+lcukyfuture@users.noreply.github.com>
2025-01-07 19:48:57 +0800

    Update and rename cachemodel.py to model.py

--- model.py
+++ model.py
@@ -1,11 +1,14 @@
 import torch
 from torch import nn
 import torch_geometric.nn as gnn
-from layer import DiffTransformerEncoderLayer
+from cachelayer import DiffTransformerEncoderLayer
 from einops import repeat
 from scipy.cluster.vq import kmeans2
 #k-means clustering to extract features from vectors each layer with more layers. 
 from timeit import default_timer as timer
+import wandb
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 
@@ -50,6 +53,16 @@ class GraphTransformer(nn.Module):
             nn.Linear(d_model, nb_class)
             )
 
+    # def log_weight_heatmap(self):
+    #     for layer in self.encoder.layers:
+    #         if hasattr(layer.self_attn, "out_proj_weight"):
+    #             weight = layer.self_attn.out_proj_weight.detach().cpu().numpy()
+    #             plt.figure(figsize=(6, 6))
+    #             sns.heatmap(weight, cmap="viridis", cbar=True)
+    #             plt.title(f"Iterations")
+    #             plt.savefig("temp_weight_heatmap.png")  
+    #             wandb.log({f"out_proj_weight_iteration_{self.iteration}": wandb.Image("temp_weight_heatmap.png")})
+    #             plt.close()
     def forward(self, x, masks, pe, lap_pe=None):
         # We permute the batch and sequence following pytorch
         # Transformer convention
